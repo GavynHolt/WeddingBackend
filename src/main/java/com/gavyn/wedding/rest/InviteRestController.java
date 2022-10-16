2055ef529b00a405495e50582c7411dec39f9ed3
Gavyn Holt <gavynholt@gmail.com>
2022-10-15 21:59:27 -0400

    initial heroku commit

--- src/main/java/com/gavyn/wedding/rest/InviteRestController.java
+++ src/main/java/com/gavyn/wedding/rest/InviteRestController.java
@@ -1,13 +1,19 @@
 package com.gavyn.wedding.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class InviteRestController {
 
     @GetMapping("test")
-    public String firstTest() {
-        return "testing 1 2 3";
+    public ResponseEntity<String> firstTest() {
+
+        return new ResponseEntity<String>( "This is a new test.", HttpStatus.OK);
     }
 }
