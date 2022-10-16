44460d5475c39e0a0bb5f7dd495127b5816c8152
Gavyn Holt <gavynholt@gmail.com>
2022-10-15 22:17:15 -0400

    fix: CORS

--- src/main/java/com/gavyn/wedding/rest/InviteRestController.java
+++ src/main/java/com/gavyn/wedding/rest/InviteRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = { "http://localhost:4200", "https://dapper-biscuit-55d875.netlify.app/"})
 public class InviteRestController {
 
     @GetMapping("test")
