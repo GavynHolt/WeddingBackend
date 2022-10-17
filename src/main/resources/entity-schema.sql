0f9101d0351cc84d6db447ff1429e024a4dd133d
Gavyn Holt <gavynholt@gmail.com>
2022-10-16 23:11:46 -0400

    fix: remove security

--- src/main/resources/entity-schema.sql
+++ src/main/resources/entity-schema.sql
@@ -1,7 +1,16 @@
 
+CREATE TABLE invitations (
+    id SERIAL primary key,
+    user_code varchar(50) unique,
+    notes text
+);
+
 CREATE TABLE guests(
     id SERIAL PRIMARY KEY NOT NULL,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    invitation_id INT REFERENCES invitations(id)
+    invitation_id INT REFERENCES invitations(id),
+    rsvp boolean,
+    rsvp_ceremony boolean,
+    rsvp_reception boolean
 );
