f0935ffc25b625e42382c0d7988ad6fd1e5e391d
Gavyn Holt <gavynholt@gmail.com>
2022-10-22 22:07:16 -0400

    fix: CORS for new site

--- src/main/java/com/gavyn/wedding/rest/InviteRestController.java
+++ src/main/java/com/gavyn/wedding/rest/InviteRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin(origins = { "http://localhost:4200", "https://gavynandjacqueline.com"})
+@CrossOrigin(origins = { "http://localhost:4200", "https://www.gavynandjacqueline.com"})
 public class InviteRestController {
 
     @Autowired
